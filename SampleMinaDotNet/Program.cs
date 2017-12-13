ef347f041bed3fda0ccfc97a1c4ba693a1d7cb4a
OH INTAEK <flytaek@gmail.com>
2017-12-13 10:35:13 +0900

    초기 커밋.

---
+++ SampleMinaDotNet/Program.cs
@@ -0,0 +1,59 @@
+﻿using Mina.Core.Service;
+using Mina.Core.Session;
+using Mina.Filter.Codec;
+using Mina.Filter.Codec.TextLine;
+using Mina.Filter.Logging;
+using Mina.Transport.Socket;
+using System;
+using System.Net;
+using System.Text;
+
+namespace SampleMinaDotNet
+{
+    class Program
+    {
+        private static readonly Int32 port = 9123;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IoAcceptor acceptor = new AsyncSocketAcceptor();
+
+                acceptor.FilterChain.AddLast("logger", new LoggingFilter());
+                acceptor.FilterChain.AddLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Encoding.UTF8)));
+
+                acceptor.ExceptionCaught += (s, e) => Console.WriteLine(e.Exception);
+                acceptor.SessionIdle += (s, e) => Console.WriteLine("IDLE " + e.Session.GetIdleCount(e.IdleStatus));
+                acceptor.MessageReceived += (s, e) =>
+                {
+                    String receiveMsg = e.Message.ToString();
+
+                    // "Quit" ? let's get out ...
+                    if (receiveMsg.Trim().Equals("quit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        e.Session.Close(true);
+                        return;
+                    }
+
+                    // Send the current date back to the client
+                    e.Session.Write(DateTime.Now.ToString());
+                    Console.WriteLine(receiveMsg);
+                };
+
+                acceptor.SessionConfig.ReadBufferSize = 2048;
+                acceptor.SessionConfig.SetIdleTime(IdleStatus.BothIdle, 10);
+
+                acceptor.Bind(new IPEndPoint(IPAddress.Any, port));
+
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ SampleMinaDotNet/TimeServerHandler.cs
@@ -0,0 +1,48 @@
+﻿using Mina.Core.Service;
+using Mina.Core.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMinaDotNet
+{
+    class TimeServerHandler : IoHandlerAdapter
+    {
+        public override void ExceptionCaught(IoSession session, Exception cause)
+        {
+            Console.WriteLine(cause);
+        }
+
+        public override void MessageReceived(IoSession session, object message)
+        {
+            base.MessageReceived(session, message);
+        }
+
+        public override void MessageSent(IoSession session, object message)
+        {
+            base.MessageSent(session, message);
+        }
+
+        public override void SessionClosed(IoSession session)
+        {
+            base.SessionClosed(session);
+        }
+
+        public override void SessionCreated(IoSession session)
+        {
+            base.SessionCreated(session);
+        }
+
+        public override void SessionIdle(IoSession session, IdleStatus status)
+        {
+            base.SessionIdle(session, status);
+        }
+
+        public override void SessionOpened(IoSession session)
+        {
+            base.SessionOpened(session);
+        }
+    }
+}
